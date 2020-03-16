27837e67363409d2d858bcf9b81a3e9c9a714dbb
hrz4app <hrz4app@gmail.com>
2020-03-16 22:23:43 +0700

    add pivot query

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('body-parser').json());
 
 app.get('/api', (req, res) => {
-    db.connect().query('SELECT * FROM store', (error, rows, fields) => {
+    db.connect().query('select * from store', (error, rows, fields) => {
+        if (error) {
+            console.log(error)
+        }
+        else {
+            res.json(rows);
+            res.end();
+        }
+    });
+});
+
+app.get('/apivot', (req, res) => {
+    db.connect().query(`select firstname, lastname, email, sum(case when (item = 'Barang1') then quantity else 0 end) as Barang1, sum(case when (item = 'Barang2') then quantity else 0 end) as Barang2, sum(case when (item = 'Barang3') then quantity else 0 end) as Barang3, sum(case when (item = 'Barang4') then quantity else 0 end) as Barang4, sum(case when (item = 'Barang5') then quantity else 0 end) as Barang5, sum(case when (item = 'Barang6') then quantity else 0 end) as Barang6, sum(case when (item = 'Barang7') then quantity else 0 end) as Barang7, sum(case when (item = 'Barang8') then quantity else 0 end) as Barang8, sum(case when (item = 'Barang9') then quantity else 0 end) as Barang9, sum(case when (item = 'Barang10') then quantity else 0 end) as Barang10 FROM store group by email`, (error, rows, fields) => {
         if (error) {
             console.log(error)
         }
